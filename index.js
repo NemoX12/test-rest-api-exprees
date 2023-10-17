35eda6845686ff136512fe695f2861b467bae6c2
Serhii Maruniak <turret2009@gmail.com>
2023-10-17 16:37:39 +0300

    changed import statements

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require("express");
 
+const users = require("./routes/api/users");
+const posts = require("./routes/api/posts");
+const comments = require("./routes/api/comments");
+
 const PORT = process.env.PORT || 3000;
 
 const app = express();
@@ -9,9 +13,9 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use("/", express.static("Public"));
 
-app.use("/api/users", require("./routes/api/users"));
-app.use("/api/posts", require("./routes/api/posts"));
-app.use("/api/comments", require("./routes/api/comments"));
+app.use("/api/users", users);
+app.use("/api/posts", posts);
+app.use("/api/comments", comments);
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}!`);
